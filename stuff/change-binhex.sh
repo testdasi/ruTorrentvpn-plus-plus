6e65f1ee32bc9110b8872255b624fd40d928b546
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-08 00:55:58 +0100

    Update change-binhex.sh

--- stuff/change-binhex.sh
+++ stuff/change-binhex.sh
@@ -3,4 +3,11 @@
 # Make changes to binhex scripts
 
 # When connection is down, pick random ovpn
-sed -i 's/openvpn_cli="\/usr\/bin\/openvpn --daemon --reneg-sec/openvpn_cli="\/usr\/bin\/openvpn --daemon --down \/temp\/random-ovpn.sh --reneg-sec/g' /root/openvpn.sh
+bhf='/root/openvpn.sh'
+echo '[info] Fix '"$bhf"
+sed -i 's/openvpn_cli="\/usr\/bin\/openvpn --daemon --reneg-sec/openvpn_cli="\/usr\/bin\/openvpn --daemon --down \/temp\/random-ovpn.sh --reneg-sec/g' "$bhf"
+
+# When openvpn is killed, pick random ovpn
+bhf='/root/getvpnport.sh'
+echo '[info] Fix '"$bhf"
+sed -i 's/kill -2 /\/bin\/bash \/temp\/random-ovpn.sh \&\& kill -2 /g' "$bhf"
