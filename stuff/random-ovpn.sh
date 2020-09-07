5fd6096addde3b6817205a9b08b6d5005ddad270
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-07 23:40:22 +0100

    Create random-ovpn.sh

---
+++ stuff/random-ovpn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# randomly pick an ovpn file from set i.e. refresh IP
+src='/config/openvpn_files'
+des='/config/openvpn'
+mkdir -p $src
+ls $src/*.ovpn | sort -R | tail -1 | while read file; do
+    cp --force "$file" "$des/openvpn.ovpn"
+done
