55d666d3f3ccbe57a685ddb2e2d0a94e6c68c427
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-07 23:55:35 +0100

    Update stubby.sh

--- stuff/stubby.sh
+++ stuff/stubby.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # create stubby config if not present and start stubby in background
+
+echo "[info] Create stubby config if not present"
 cd /temp && mkdir -p /config/stubby && cp -n ./stubby.yml /config/stubby/ && rm -f ./stubby.yml
+
+echo "[info] Run stubby in background"
 stubby -g -C /config/stubby/stubby.yml
