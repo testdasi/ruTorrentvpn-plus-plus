0db0605c11b99a042e97f442b3a9f83be81279ac
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-07 23:41:16 +0100

    Update startup.sh

--- stuff/startup.sh
+++ stuff/startup.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# randomly pick an ovpn file from set i.e. refresh IP
-src='/config/openvpn_files'
-des='/config/openvpn'
-mkdir -p $src
-ls $src/*.ovpn | sort -R | tail -1 | while read file; do
-    cp --force "$file" "$des/openvpn.ovpn"
-done
+/bin/bash /temp/random-ovpn.sh
 
-# create stubby config if not present and start stubby in background
-cd /temp && mkdir -p /config/stubby && cp -n ./stubby.yml /config/stubby/ && rm -f ./stubby.yml
-stubby -g -C /config/stubby/stubby.yml
+/bin/bash /temp/stubby.sh
 
 # binhex init script
 /bin/bash /usr/local/bin/init.sh
