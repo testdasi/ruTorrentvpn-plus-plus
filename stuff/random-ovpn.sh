0577924bb1c901c7e454ca0acb7a19ddd4318373
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-08 01:18:30 +0100

    Update random-ovpn.sh

--- stuff/random-ovpn.sh
+++ stuff/random-ovpn.sh
@@ -6,6 +6,7 @@ echo "[info] Pick a random ovpn file"
 src='/config/openvpn_files'
 des='/config/openvpn'
 mkdir -p $src
+ls $src/*.ovpn | sort -R
 ls $src/*.ovpn | sort -R | tail -1 | while read file; do
     cp --force "$file" "$des/openvpn.ovpn"
     echo "[info] "${file}" picked"
