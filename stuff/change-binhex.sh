f8f5522accac6f1d63d0aa35ea4efbad24bdd089
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-08 02:04:59 +0100

    Update change-binhex.sh

--- stuff/change-binhex.sh
+++ stuff/change-binhex.sh
@@ -3,14 +3,15 @@
 # Make changes to binhex scripts
 
 # When connection is down, pick random ovpn
-bhf='/root/openvpn.sh'
-echo '[info] Fix '"$bhf"
+#bhf='/root/openvpn.sh'
+#echo '[info] Fix '"$bhf"
 #sed -i 's/openvpn_cli="\/usr\/bin\/openvpn --daemon --reneg-sec/openvpn_cli="\/usr\/bin\/openvpn --daemon --down \/temp\/random-ovpn.sh --reneg-sec/g' "$bhf"
-sed -i 's/		pkill /		\/bin\/bash \/temp\/random-ovpn.sh \&\& pkill /g' "$bhf"
+#sed -i 's/		pkill /		\/bin\/bash \/temp\/random-ovpn.sh \&\& pkill /g' "$bhf"
 #sed -i 's/		eval /		\/bin\/bash \/temp\/random-ovpn.sh \&\& eval /g' "$bhf"
 
-# When openvpn is killed, pick random ovpn
 bhf='/root/getvpnport.sh'
 echo '[info] Fix '"$bhf"
+# When error from getvpn port, pick random ovpn
 sed -i 's/		kill -2 /		\/bin\/bash \/temp\/random-ovpn.sh \&\& kill -2 /g' "$bhf"
+# Reduce number of retries from 12 to 3
 sed -i 's/		curly.sh -ct 10 -rc 12 /		curly.sh -ct 10 -rc 3 /g' "$bhf"
