098b94e6707a9337e00755b6be3d5df13cf8064a
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-08 01:57:26 +0100

    Update change-binhex.sh

--- stuff/change-binhex.sh
+++ stuff/change-binhex.sh
@@ -7,9 +7,10 @@ bhf='/root/openvpn.sh'
 echo '[info] Fix '"$bhf"
 #sed -i 's/openvpn_cli="\/usr\/bin\/openvpn --daemon --reneg-sec/openvpn_cli="\/usr\/bin\/openvpn --daemon --down \/temp\/random-ovpn.sh --reneg-sec/g' "$bhf"
 sed -i 's/		pkill /		\/bin\/bash \/temp\/random-ovpn.sh \&\& pkill /g' "$bhf"
-sed -i 's/		eval /		\/bin\/bash \/temp\/random-ovpn.sh \&\& eval /g' "$bhf"
+#sed -i 's/		eval /		\/bin\/bash \/temp\/random-ovpn.sh \&\& eval /g' "$bhf"
 
 # When openvpn is killed, pick random ovpn
 bhf='/root/getvpnport.sh'
 echo '[info] Fix '"$bhf"
 sed -i 's/		kill -2 /		\/bin\/bash \/temp\/random-ovpn.sh \&\& kill -2 /g' "$bhf"
+sed -i 's/		curly.sh -ct 10 -rc 12 /		curly.sh -ct 10 -rc 3 /g' "$bhf"
