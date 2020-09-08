77aa1cd784678c7c5a32d7fec92db5be249a4d0c
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-08 01:37:14 +0100

    Update change-binhex.sh

--- stuff/change-binhex.sh
+++ stuff/change-binhex.sh
@@ -5,9 +5,11 @@
 # When connection is down, pick random ovpn
 bhf='/root/openvpn.sh'
 echo '[info] Fix '"$bhf"
-sed -i 's/openvpn_cli="\/usr\/bin\/openvpn --daemon --reneg-sec/openvpn_cli="\/usr\/bin\/openvpn --daemon --down \/temp\/random-ovpn.sh --reneg-sec/g' "$bhf"
+#sed -i 's/openvpn_cli="\/usr\/bin\/openvpn --daemon --reneg-sec/openvpn_cli="\/usr\/bin\/openvpn --daemon --down \/temp\/random-ovpn.sh --reneg-sec/g' "$bhf"
+sed -i 's/		pkill /		\/bin\/bash \/temp\/random-ovpn.sh \&\& pkill /g' "$bhf"
+sed -i 's/		eval /		\/bin\/bash \/temp\/random-ovpn.sh \&\& eval /g' "$bhf"
 
 # When openvpn is killed, pick random ovpn
 bhf='/root/getvpnport.sh'
 echo '[info] Fix '"$bhf"
-sed -i 's/kill -2 /\/bin\/bash \/temp\/random-ovpn.sh \&\& kill -2 /g' "$bhf"
+sed -i 's/		kill -2 /		\/bin\/bash \/temp\/random-ovpn.sh \&\& kill -2 /g' "$bhf"
