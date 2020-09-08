1cefbc55f4bfb8437d6e09b55dabe63e9f175e9e
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-08 11:14:38 +0100

    Comment out my fix binhex script

--- stuff/startup.sh
+++ stuff/startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+#Attempt to fix binhex scripts
 #/bin/bash /temp/change-binhex.sh
+
+#random VPN picker
 /bin/bash /temp/random-ovpn.sh
+
+#stubby initialisation
 /bin/bash /temp/stubby.sh
 
 # binhex init script
